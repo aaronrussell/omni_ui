9199aa3d14c4319d3db7f77b1cc59c959060dda7
aaronrussell <124721263+aaronrussell@users.noreply.github.com>
2026-03-13 12:19:04 +0000

    Initial commit. mix new

---
+++ lib/omni_ui.ex
@@ -0,0 +1,18 @@
+defmodule OmniUI do
+  @moduledoc """
+  Documentation for `OmniUI`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> OmniUI.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/omni_ui_test.exs
@@ -0,0 +1,8 @@
+defmodule OmniUITest do
+  use ExUnit.Case
+  doctest OmniUI
+
+  test "greets the world" do
+    assert OmniUI.hello() == :world
+  end
+end
